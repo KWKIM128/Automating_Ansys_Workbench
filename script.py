f131545ea33a4fbfbf2d7e87f45ec877e13e7691
Keun Woo Kim <115262940+KWKIM128@users.noreply.github.com>
2023-02-27 21:16:48 +0000

    Create script.py

---
+++ script.py
@@ -0,0 +1,34 @@
+# Finding the failing stress
+# sopy paste this code to ansys script
+
+Model = ExtAPI.DataModel.Project.Model
+Geometry = Model.Geometry
+Material = Model.Materials
+CS = Model.CoordinateSystems
+Mesh = Model.Mesh
+Static_Sol = Analysis.Solution
+UTS = 45 # in MPa. 
+
+# inital force
+Force = -1000 # change to the force you found but don't change the negative sign
+
+
+# Do not change code below
+while(True):
+    Load = Analysis.AddForce()
+    Load.Location = ExtAPI.DataModel.GetObjectsByName("Load")[0]
+    Load.DefineBy = LoadDefineBy.Components
+    Load.XComponent.Output.SetDiscreteValue(0, Quantity(Force, "N"))
+   
+    Eqv = Static_Sol.AddEquivalentStress()
+    Static_Sol.Solve(True)
+    analysis = Model.Analyses[0]
+    reader = analysis.GetResultsData()
+    stress = reader.GetResult("S")
+    stress.SelectComponents(["X"])
+    Sx = stress.GetElementValues(1)
+   
+    if Sx >= UTS:
+        break
+    else:
+        Force-=100
