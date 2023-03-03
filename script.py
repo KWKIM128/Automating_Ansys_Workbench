fd1054cf38a03146e9790ece2230bba1bfe1bdfd
Keun Woo Kim <115262940+KWKIM128@users.noreply.github.com>
2023-03-03 12:09:09 +0000

    Update script.py

--- script.py
+++ script.py
@@ -11,10 +11,10 @@ UTS = 45 # in MPa.
 
 # inital force
 Force = -700 # change to the force to the value you found but don't change the negative sign
-
+Fmax = -2000
 
 # Do not change code below
-while(True):
+while(Force < Fmax):
     Load = Analysis.AddForce()
     Load.Location = ExtAPI.DataModel.GetObjectsByName("Load")[0]
     Load.DefineBy = LoadDefineBy.Components
@@ -22,13 +22,3 @@ while(True):
    
     Eqv = Static_Sol.AddEquivalentStress()
     Static_Sol.Solve(True)
-    analysis = Model.Analyses[0]
-    reader = analysis.GetResultsData()
-    stress = reader.GetResult("S")
-    stress.SelectComponents(["X"])
-    Sx = stress.GetElementValues(1)
-   
-    if Sx >= UTS:
-        break
-    else:
-        Force-=100
